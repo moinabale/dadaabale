b93966374cb8b07bfb5ab900c4a1e5ae61c5d4d5
KHWAJAMOIN ABALE <167965225+moinabale@users.noreply.github.com>
2024-05-13 23:00:24 +0530

    Add files via upload

---
+++ factorial.java
@@ -0,0 +1,12 @@
+public class factorial {
+    public static void main(String args[]){
+        int i,fact=1;
+        int num=5;
+        for(i=1;i<=num;i++){
+            fact=fact*i;
+        }
+        System.out.println("factorial of given number is:"+fact);
+        }
+    }
+    
+
